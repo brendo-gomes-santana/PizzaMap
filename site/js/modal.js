4e49701e96e33fbed36dfae5025c82ac2b7d69c6
Moisés Clemente <130271950+clementscompany@users.noreply.github.com>
2023-09-09 11:41:17 +0100

    Tela de cadastro em  Andamento
    tela de cadastro!

--- site/js/modal.js
+++ site/js/modal.js
@@ -1,5 +1,16 @@
 // modal code ...
-const closeBtn = document.querySelector();
-closeBtn.onclick = () =>{
-    
-}
+
+ const container = document.querySelector(`[mainContainer="content"]`);
+ const buttonOpen = container.querySelector(".openBtn")
+ const closeBtn =  container.querySelector(".closeBtn");
+
+//  buttonOpen.onclick  = ()=>{
+//     let modal = container.querySelector(".modalWindow")
+//     modal.classList.add('active');
+//  }
+
+closeBtn.onclick = ()=>{
+    let modal = container.querySelector(".modalWindow")
+    modal.classList.remove('active');
+ }
+
