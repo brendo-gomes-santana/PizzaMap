5ecf573dc7742c4f7185dfcdc4513aaa1bd88745
Moisés Clemente <130271950+clementscompany@users.noreply.github.com>
2023-09-11 17:50:30 +0100

    Parte 2 tela de cadastro! -Moisés
    Commint

--- site/js/modal.js
+++ site/js/modal.js
@@ -3,12 +3,16 @@
  const container = document.querySelector(`[mainContainer="content"]`);
  const buttonOpen = container.querySelector(".openBtn")
  const closeBtn =  container.querySelector(".closeBtn");
+ const InputImage =  container.querySelector("#inputFile");
 
 //  buttonOpen.onclick  = ()=>{
 //     let modal = container.querySelector(".modalWindow")
 //     modal.classList.add('active');
 //  }
-
+InputImage.onchange = ()=>{
+   let ckeckImage =  document.querySelector(".ckeckImage");
+   ckeckImage.classList.add('active');
+}
 closeBtn.onclick = ()=>{
     let modal = container.querySelector(".modalWindow")
     modal.classList.remove('active');
